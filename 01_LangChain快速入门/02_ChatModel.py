adec4eb16894736e539dcf6ec11f27c03e48adc9
ChowRunFa <wtqiu@qq.com>
2024-03-07 17:15:37 +0800

    glm

--- 01_LangChain快速入门/02_ChatModel.py
+++ 01_LangChain快速入门/02_ChatModel.py
@@ -2,17 +2,11 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
-from langchain.chat_models import ChatOpenAI
-chat = ChatOpenAI(model="gpt-3.5-turbo",
-                    temperature=0.8,
-                    max_tokens=60)
-from langchain.schema import (
-    HumanMessage,
-    SystemMessage
-)
-messages = [
-    SystemMessage(content="你是一个很棒的智能助手"),
-    HumanMessage(content="请给我的花店起个名")
-]
-response = chat(messages)
+from load_glm import llm
+model = llm
+
+response = model("请写一篇滕王阁序")
+
 print(response)
+
+
